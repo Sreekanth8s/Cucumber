5b99483a40864c4deb8003b69c559af572e60874
sreek <sreek@LAPTOP-HGCFQ4DS>
2022-02-02 14:58:11 +0530

    Commit:Added Test Case1

---
+++ sample data/src/TestCase1/Testcase1.java
@@ -0,0 +1,12 @@
+package TestCase1;
+
+public class Testcase1 {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("git sucesfull");
+
+	}
+
+}
